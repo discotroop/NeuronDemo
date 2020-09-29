a0b37887f9022a237fb40f7abc530a59678378ee
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-29 19:31:09 -0400

    Updates

---
+++ precache-manifest.46f366def80dc703df814900e9fb7825.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187587c49322b36dfb58bac40e41e75f",
+    "url": "/NeuronDemo/index.html"
+  },
+  {
+    "revision": "29f78e2ec9baa472b155",
+    "url": "/NeuronDemo/static/css/2.1b4b65d2.chunk.css"
+  },
+  {
+    "revision": "171c3e17bdf55f617cd0",
+    "url": "/NeuronDemo/static/css/main.4ecb3f0c.chunk.css"
+  },
+  {
+    "revision": "29f78e2ec9baa472b155",
+    "url": "/NeuronDemo/static/js/2.e5b39d1e.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/NeuronDemo/static/js/2.e5b39d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171c3e17bdf55f617cd0",
+    "url": "/NeuronDemo/static/js/main.38faaf46.chunk.js"
+  },
+  {
+    "revision": "c6e74f8feb02c48e05a6",
+    "url": "/NeuronDemo/static/js/runtime-main.e5777b67.js"
+  },
+  {
+    "revision": "9bcf1dc03ceaea12f1e4d913a33937fc",
+    "url": "/NeuronDemo/static/media/info.9bcf1dc0.png"
+  }
+]);
